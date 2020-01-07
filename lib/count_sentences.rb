d23f6dc2bb94c5ca3aa1c7ce76dc3df7e64f6c03
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-07 15:21:53 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,6 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).delete_if{|sentence| sentence.length < 2}.count 
+    self.split(/[.!?]/).delete_if{|sentence| /[^\w]/}.count 
   end
 end
